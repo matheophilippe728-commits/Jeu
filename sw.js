c61da2bedd10669dc027410563ebdf3c5823e4fb
matheophilippe728-commits <matheophilippe728@gmail.com>
2026-01-04 05:28:14 +0100

    Refactor service worker for new version and cleanup
    Updated service worker to new version and removed old caching logic.

--- sw.js
+++ sw.js
@@ -1,27 +1,10 @@
-const CACHE_NAME = "civitas-v1";
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/sw.js",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
+// Force iOS à prendre la nouvelle version
+const VERSION = "civitas-sw-v3";
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((c) => c || fetch(event.request)));
+  event.waitUntil(self.clients.claim());
 });
